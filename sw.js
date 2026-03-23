ac64e4a25cd3cb280e946a4ea7c5f26a3b5849c6
Trent Buckley <trentbuckley6699@gmail.com>
2026-03-23 22:08:23 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,25 @@
-self.addEventListener('fetch', function(event) {
-  // This can be empty for a basic PWA, 
-  // but it must exist to trigger the install prompt.
+const CACHE_NAME = 'mhs-tools-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './logo192.png',
+  './logo512.png'
+];
+
+// Install the service worker and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Serve assets from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
